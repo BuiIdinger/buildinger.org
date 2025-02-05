5f286f16b37310d016520261e91fe220a6ff7160
Shayden seymour <shaydenseymour@gmail.com>
2025-02-05 17:47:08 +1300

    Comments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+
+  /* Enable dev tools */
   devtools: { enabled: true },
 
+  /* Add modules */
   modules: [
     "@nuxtjs/tailwindcss",
   ],
+
+  /* Disable auto imports */
+  imports: {
+    autoImport: false
+  }
 })
